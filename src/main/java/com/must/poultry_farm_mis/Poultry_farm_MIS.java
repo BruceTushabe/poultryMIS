d570c93a9130d07541eaca7ae94cdd8cbdeb9c6d
Bruce Tushabe <brucetushabe@gmail.com>
2023-04-02 16:51:25 +0300

    Lets build a Poultry MIS

---
+++ src/main/java/com/must/poultry_farm_mis/Poultry_farm_MIS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.must.poultry_farm_mis;
+
+/**
+ *
+ * @author MY PC
+ */
+public class Poultry_farm_MIS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
